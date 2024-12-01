218a8756574d4b16afb236d34ad7021ccc215d71
Turkuvaz <74011374+turkuvazbiri@users.noreply.github.com>
2024-12-01 14:56:59 +0300

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,40 @@
+import numpy as np
+import random as rd
+
+# This function include the random matrix generator with numpy and random library.
+# First argument defines the size of matrix| for example if you want 3x3 -> randomnp(3,start,end).
+# Second and third arguments is the range of random integers, both arguments included.
+
+def rdmatrix(size,start,end):
+    if type(size) == int and type(start) == int and type(end) == int:
+        pid = 1 # The process number variable as integer.
+        matrixtxt = "" # The variable that provides us to np.asmatrix's first argument as string.
+        while pid<size*size+1:
+            if not pid == size*size: # This if statements for text formatting in np.asmatrix function.
+                if pid % size == 0:
+                    matrixtxt+=str(rd.randint(start,end)) + "; " 
+                else:
+                    matrixtxt+=str(rd.randint(start,end)) + " " 
+            else:
+                matrixtxt+=str(rd.randint(start,end))
+            pid+=1
+        return np.array(np.asmatrix(matrixtxt))
+    else:
+        return print("TypeError: Please enter valid types.") # Type Check
+
+# This function include the matrix array's diagonal summation.
+# Takes as first argument the np.ndarray type and returns the INTEGER type.
+
+def diagsumMatrix(arr): 
+    if type(arr) == np.ndarray: 
+        diagonarr = arr.diagonal() # Basically uses the combination of diagonal and summation method.
+        result = diagonarr.sum()
+        return result
+    else:
+        return print("TypeError: Please enter a valid type.") # Type Check
+
+# Debug codes:
+# myArray = rdmatrix(10,1,100)
+# print(myArray)
+# sum = diagsumMatrix(myArray)
+# print(sum)
